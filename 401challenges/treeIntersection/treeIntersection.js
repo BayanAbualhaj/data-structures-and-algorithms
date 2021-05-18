5ec8202bc3a2e5c379ccae3ba07b14bdae2e9245
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-05-18 13:51:33 +0300

    re-solve

--- 401challenges/treeIntersection/treeIntersection.js
+++ 401challenges/treeIntersection/treeIntersection.js
@@ -3,7 +3,19 @@
 function treeIntersect(tree1, tree2) {
   const tree1Arr = tree1.postOrder();
   const tree2Arr = tree2.postOrder();
-  const intersectedArr = tree1Arr.filter(element => tree2Arr.includes(element));
+  const intersectedArr = [];
+  let k=0;
+
+  for (let i = 0; i < tree1Arr.length; i++) {
+    for (let j = 0; j < tree2Arr.length; j++) {
+        if (tree1Arr[i]===tree2Arr[j]) {
+            intersectedArr[k]=tree1Arr[i];
+            k++;
+        }
+        
+    }
+    
+}
 
   if(intersectedArr.length){
       return intersectedArr;
